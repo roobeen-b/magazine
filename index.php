b2dd12f5706726903ec1716ca75df1f4654f5063
roobeen-b <rubinbaidhya@gmail.com>
2020-05-23 14:29:36 +0545

    Database module for project

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-  ob_start();
-  @header("location: cms/index");
+  //ob_start();
+  include $_SERVER['DOCUMENT_ROOT'].'config/init.php';
+
+  //redirect('cms/index');
+ // @header("location: cms/index");
+
+  $user = new user();
+  $data = array(
+  	'username' => 'rubin',
+  	'session_token' => tokenizer()
+  );
+  $user->deleteUserByEmail('rubin@magazine.com');
  ?>
